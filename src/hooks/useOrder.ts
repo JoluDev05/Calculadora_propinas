b59a6b18c3f4a754284d0f545e25b81a3cfc2a41
Jorge Luis Arce <jorgearcedev1@gmail.com>
2025-06-13 14:21:52 -0700

    Evitando duplicados

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -1,12 +1,27 @@
 import {useState} from 'react';
-import type { OrderItem } from '../types';
+import type { MenuItem, OrderItem } from '../types';
 
 export default function useOrder() {
     const [order, setOrder] = useState<OrderItem[]>([])
 
-    const addItem = ()=> {
-        console.log('agregando... ');
+    const addItem = (item : MenuItem)=> {
+        const itemExist = order.find(orderItem => orderItem.id === item.id)
+        if (itemExist) {
+            // If the item already exists, increase the quantity
+            const updatedOrder = order.map(orderItem => 
+                orderItem.id === item.id 
+                ? {...orderItem, quantity: orderItem.quantity + 1} 
+                : orderItem
+            );
+            setOrder(updatedOrder);
+        } else {
+            // If the item does not exist, add it to the order with quantity 1
+            const newItem = {...item, quantity: 1}
+            setOrder([...order, newItem]);
+        }
     }
+
+    console.log(order);
 
     return {
         addItem
